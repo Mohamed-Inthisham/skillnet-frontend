6d890f64ea88ce0976568b83681f3ff28de29ed3
chami-gunawardane <chamigunawardane@gmail.com>
2025-03-11 16:16:05 +0530

    Completed EnglishFluency Screen with count down

--- src/pages/EnglishFluency/index.jsx
+++ src/pages/EnglishFluency/index.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import UserHeader from "../../layout/UserHeader";
+import Footer from "../../layout/Footer";
 
-function index() {
+const EnglishFluency = () => {
+  const [counter, setCounter] = useState(5);
+
+  useEffect(() => {
+    if (counter > 0) {
+      const timer = setTimeout(() => setCounter(counter - 1), 1000);
+      return () => clearTimeout(timer); 
+    }
+  }, [counter]);
+
   return (
-    <div>index</div>
-  )
-}
+    <div className="flex flex-col min-h-screen font-[Poppins]">
+      <UserHeader />
 
-export default index
+      <main className="flex flex-grow items-center justify-center px-6 py-10">
+        <div className="w-full max-w-3xl h-[500px] bg-white shadow-lg rounded-lg p-8 border border-gray-200 flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-medium text-gray-800 text-center">
+            English Fluency Test
+          </h1>
+          <p className="text-md text-gray-600 mt-2 text-center">
+            Please speak for more than 1 minute
+          </p>
+          <p className="text-md text-gray-600 text-center">
+            Record Starting in.....
+          </p>
+
+          {/* Countdown Timer */}
+          <div className="mt-6">
+            <span className="countdown font-mono text-6xl">
+              <span
+                style={{ "--value": counter }}
+                aria-live="polite"
+                aria-label={counter}
+              >
+                {counter}
+              </span>
+            </span>
+          </div>
+        </div>
+      </main>
+
+      <Footer bgColor="bg-black" textColor="text-white" />
+    </div>
+  );
+};
+
+export default EnglishFluency;
