c6b70d66e2fe9cb8c881618cf319f40a23a3b8c4
chami-gunawardane <chamigunawardane@gmail.com>
2025-03-12 13:02:12 +0530

    Fixed page links

--- src/components/Home/HeroSection/index.jsx
+++ src/components/Home/HeroSection/index.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import Button from "../../Button";
 import HeroImage from "../../../assets/HeroImage.webp";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => (
   <section className="flex justify-between items-center pl-10 mt-10 bg-white border-2 border-gray-200 rounded-lg w-[1000px] ml-[200px] font-[Poppins]">
     <div>
       <h1 className="text-3xl font-semibold -mt-12">
-        Elevate your <br/> expertise. Learn from <br /> top companies.
+        Elevate your <br /> expertise. Learn from <br /> top companies.
       </h1>
       <p className="text-gray-500 mt-5">
-        Level up your skills and supercharge your career with our transformative programs.
+        Level up your skills and supercharge your career with our transformative
+        programs.
       </p>
-      <Button text="Explore Programs" variant="outline" className="mt-4" />
+      <Link to="/Programs">
+        <Button
+          text="Explore Programs"
+          variant="outline"
+          className="mt-4 cursor-pointer"
+        />
+      </Link>
     </div>
-    <img src={HeroImage} alt="Hero" className="w-2/3 rounded-rt-lg rounded-rb-lg h-[350px]" />
+    <img
+      src={HeroImage}
+      alt="Hero"
+      className="w-2/3 rounded-rt-lg rounded-rb-lg h-[350px]"
+    />
   </section>
 );
 
